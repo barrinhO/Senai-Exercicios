df343f2e25173bc02aca0b9534beab635ffa5647
CHRISTIAN MOREIRA DE BARROS <christian_m_barros@estudante.sesisenai.org.br>
2025-02-18 15:27:39 -0300

    loop while exemplo

--- teste.py
+++ teste.py
@@ -1,16 +1,12 @@
-#Exercício 2 18-02-25
+# loops WHILE
 
 
-seu_nome = str(input("Digite seu nome: "))
+condicao = True
+numero = 0
 
-if(len(seu_nome) <= 4):
-    print("Seu nome é curto!")
-    
-elif(len(seu_nome) == 5 or len(seu_nome) == 6 ):
-    print("Seu nome é normal!")
-    
-elif(len(seu_nome) ==    9):
-    print("Seu nome é muito grande")
-    
-else:
-    print("Seu nome é gigantee meu nobre!")
+while condicao:
+    numero = int(input("Num: "))
+    if(numero == str(numero)):
+        break
+    print(numero)
+print(condicao)
