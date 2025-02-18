f1bff5c6334c665b4a203103ccb091bed117effe
CHRISTIAN MOREIRA DE BARROS <christian_m_barros@estudante.sesisenai.org.br>
2025-02-18 16:21:24 -0300

    Exercicios com for, while e uma calculadora básica

--- teste.py
+++ teste.py
@@ -1,12 +1,46 @@
-# loops WHILE
+# exercicio 1 for
+string = "abóbora"
+print(f"Primeira string: {string}")
+char = string.split(" ")
+print("Segunda string: ")
+for char in string:
+    print(f"*{char}", end="")
 
 
-condicao = True
-numero = 0
+#While 
 
-while condicao:
-    numero = int(input("Num: "))
-    if(numero == str(numero)):
-        break
-    print(numero)
-print(condicao)
+texto1 = input("texto: ")
+texto2 = "*"
+indice = 0
+while indice < len(texto1):
+    texto2 += texto2[indice]+"*"
+    indice += 1
+print(texto2)
+    
+    
+# exercício 2
+
+num1 = int(input("Digite um número: "))
+num2 = int(input("Digite outro número: "))
+soma = num1 + num2
+sub =  num1 - num2
+mult = num1 * num2
+div = num1 / num2
+
+print("Utilize 1 para somar")
+print("Utilize 2 para subtrair")
+print("Utilize 3 para multiplicar")
+print("Utilize 4 para dividir")
+
+operacao = str(input("Qual operação você deseja fazer? "))
+
+if(operacao == '1'):
+    print(f"Soma: {soma}")
+elif(operacao == '2'):
+    print(f"Subtração: {sub}")
+elif(operacao == '3'):
+    print(f"Multiplicação: {mult}")
+elif(operacao == '4'):
+    print(f"Divisão: {div}")
+else:
+    print("Por favor digite o numero da operação")
