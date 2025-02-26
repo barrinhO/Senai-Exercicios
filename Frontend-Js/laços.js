1738f2aa28a1e40a9beae56a4d2e477e7388bd13
Christian Moreira <155912614+barrinhO@users.noreply.github.com>
2025-02-26 17:13:29 -0300

    Update laços.js

--- Frontend-Js/laços.js
+++ Frontend-Js/laços.js
@@ -1,4 +1,4 @@
-console.log('Números pares:');
+console.log("Números pares:");
 let num = 1;
 while (num <= 10) {
     if (num % 2 == 0) {
@@ -8,7 +8,7 @@ while (num <= 10) {
     //num--; de trás para frente
 }
 
-console.log('Números ímpares:');
+console.log("Números ímpares:");
 let num2 = 1;
 while (num2 <= 10) {
     if (num2 % 2 != 0) {
@@ -18,4 +18,27 @@ while (num2 <= 10) {
     //num2--; de trás para frente
 }
 
-// For
+let nome = "",
+    nomes = "";
+
+nome += prompt("Digite um nome") + ", ";
+nomes += nome + ", ";
+
+while (nome != "pare" && nome != "PARE") {
+    nome = prompt("Digite um nome");
+    nomes += nome + ", ";
+}
+
+alert(nomes);
+
+let valor,
+    total = 0;
+
+valor = parseInt(prompt("Digite um valor"));
+
+while (valor != 0) {
+    total += valor;
+    valor = parseInt(prompt("Digite um valor"));
+}
+
+alert(total);
